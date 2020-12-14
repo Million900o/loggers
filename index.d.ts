2fefd0c757f4aaf7523e5056bc9f2b1b05a11e3e
Million900o <apaolini900o@gmail.com>
2020-12-13 23:26:16 -0500

    update typings

--- index.d.ts
+++ index.d.ts
@@ -1,23 +1,23 @@
 declare module 'loggers' {
   interface LoggerOptions {
-    debug: boolean,
-    method: any,
-    colors: boolean,
-    newLine: boolean,
-    catch: boolean;
+    debug?: boolean,
+    method?: any,
+    colors?: boolean,
+    newLine?: boolean,
+    catch?: boolean;
   }
 
   export class Logger {
     constructor(options: LoggerOptions);
     private send: any;
     public readonly options: LoggerOptions;
-    private _formatArgs(): string;
-    private _formatColors(): string;
+    private _formatArgs(args: string[]): string;
+    private _formatColors(text: string, color: any): string;
     private _newLine(): string;
-    public log(): boolean;
-    public warn(): boolean;
-    public error(): boolean;
-    public err(): boolean;
-    public debug(): boolean;
+    public log(...args: any[]): boolean;
+    public warn(...args: any[]): boolean;
+    public error(...args: any[]): boolean;
+    public err(...args: any[]): boolean;
+    public debug(...args: any[]): boolean;
   }
 }
